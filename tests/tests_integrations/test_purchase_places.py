be786193ae00caa199124cd5d5ece4bb51bfbc94
antogro <153613140+antogro@users.noreply.github.com>
2025-01-02 10:50:34 +0100

    ajout du contrôle des points (clubs, competitions), retour calcul du point des clubs, controle des données de l'input utilisateur dans le template booking

--- tests/tests_integrations/test_purchase_places.py
+++ tests/tests_integrations/test_purchase_places.py
@@ -6,9 +6,10 @@ def test_purchase_place_with_valide_data(client, data):
         "club": club['name'],
         "places": places
     })
+    decoded_response = response.data.decode('utf-8')
 
     assert response.status_code == 200
-    assert b'Reservation Confirme'
+    assert 'Réservation confirmée !' in decoded_response
 
 
 def test_purchase_place_with_invalide_competition(client, places, club):
@@ -21,7 +22,25 @@ def test_purchase_place_with_invalide_competition(client, places, club):
             'club': club['name'],
             'places': places
         })
+    decoded_response = response.data.decode('utf-8')
     assert response.status_code == 400
+    assert 'Compétition introuvable. Veuillez réessayer.' in decoded_response
+
+
+def test_purchase_place_with_invalide_club(client, places, competition):
+    club = {'name': 'Invalid club'}
+
+    response = client.post(
+        '/purchase_places',
+        data={
+            'competition': competition['name'],
+            'club': club['name'],
+            'places': places
+        })
+    decoded_response = response.data.decode('utf-8')
+    print(decoded_response)
+    assert response.status_code == 400
+    assert 'Nom du Club introuvable. Veuillez reessayer.' in decoded_response
 
 
 def test_purchase_place_with_to_more_place_than_competition_places(
@@ -37,40 +56,86 @@ def test_purchase_place_with_to_more_place_than_competition_places(
             'places': places
         }
     )
+    decoded_response = response.data.decode('utf-8')
     assert response.status_code == 400
-    assert b'Vous ne pouvez reserver plus que place que celle disponible.'
+    assert ('Vous ne pouvez réserver plus de places que celles disponibles.' in
+            decoded_response)
 
 
 def test_purchase_place_with_more_than_12_places(
         client,
-        setup_app
+        setup_app_competition_more_place
 ):
     places = 13
+    response = client.post(
+        '/purchase_places',
+        data={
+            'competition': setup_app_competition_more_place.competition[0]['name'],
+            'club': setup_app_competition_more_place.club[0]['name'],
+            'places': places
+        }
+    )
+    decoded_response = response.data.decode('utf-8')
+    assert response.status_code == 400
+    assert (
+        'Vous ne pouvez réserver plus de 12 places sur une compétition.'
+        in decoded_response
+        )
+
+
+def test_purchase_place_with_insufficient_competition_places(client, setup_app):
+    places = 14  # Plus que les places disponibles
     response = client.post(
         '/purchase_places',
         data={
             'competition': setup_app.competition[0]['name'],
             'club': setup_app.club[0]['name'],
             'places': places
-            }
+        }
     )
+    decoded_response = response.data.decode('utf-8')
     assert response.status_code == 400
-    assert b'Vous ne pouvez reserver plus de 12 places.'
+    assert (
+        'Vous ne pouvez réserver plus de places que celles disponibles.'
+        in decoded_response
+        )
 
 
-def test_purchase_place_without_enought_club_point(
+def test_purchase_place_with_insufficient_club_points(
         client,
         setup_app_club_low_place
 ):
-    places = 6
-    print('competition', setup_app_club_low_place.competition[0]['name'], 'club', setup_app_club_low_place.club_test[0]['name'])
+    places = 6  # Plus que les points disponibles du club
     response = client.post(
         '/purchase_places',
         data={
             'competition': setup_app_club_low_place.competition[0]['name'],
             'club': setup_app_club_low_place.club_test[0]['name'],
             'places': places
-            }
+        }
     )
+    decoded_response = response.data.decode('utf-8')
+    assert response.status_code == 400
+    assert (
+        'Vous ne pouvez réserver plus de places que vos points disponibles.'
+        in decoded_response
+        )
+
+
+def test_points_deduction_after_purchase(client, setup_app, data):
+    competition, club, places = data
+
+    response = client.post('/purchase_places', data={
+        "competition": competition['name'],
+        "club": club['name'],
+        "places": places
+    })
+    decoded_response = response.data.decode('utf-8')
+
     assert response.status_code == 200
-    assert b'Vous ne pouvez reserver plus de places que le club en possedes.'
+    assert 'Réservation confirmée !' in decoded_response
+
+    # Vérification des points.
+    assert 'Number of Places: 3' in decoded_response
+    assert 'Points available: 2' in decoded_response
+
